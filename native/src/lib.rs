dc1aceb53346c42b68899a10f4f891eea7ebbce9
mycognosist <gnomad@cryptolab.net>
2021-04-07 14:16:03 +0000

    Remove manual array validation method and rename value methods

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -52,35 +52,6 @@ fn validate_message_value(mut cx: FunctionContext) -> JsResult<JsString> {
     }
 }
 
-// Validate a chain of messages.
-// Expects an argument in the form of an array of JSON objects.
-fn validate_message_chain(mut cx: FunctionContext) -> JsResult<JsString> {
-    let arg1 = cx.argument::<JsValue>(0)?;
-    let arg2 = cx.argument::<JsValue>(1)?;
-
-    // TODO: improve type checking and error-handling
-    if arg1.is_a::<JsUndefined>() || arg1.is_a::<JsNull>() || arg2.is_a::<JsUndefined>() {
-        panic!("undefined argument(s)")
-    };
-
-    // previous message
-    let msg1 = utils::json_stringify(&mut cx, ArrayVec::from([arg1]))?
-        .value()
-        .into_bytes();
-
-    // current message
-    let msg2 = utils::json_stringify(&mut cx, ArrayVec::from([arg2]))?
-        .value()
-        .into_bytes();
-
-    let messages = [msg1, msg2];
-
-    match ssb_validate::par_validate_message_hash_chain_of_feed::<_, &[u8]>(&messages, None) {
-        Ok(_) => Ok(cx.string("validated")),
-        Err(e) => panic!("{}", e),
-    }
-}
-
 // Return number of items in array.
 fn array_item_count(mut cx: FunctionContext) -> JsResult<JsString> {
     // take the first argument, which must be an array
@@ -113,9 +84,8 @@ fn validate_message_array(mut cx: FunctionContext) -> JsResult<JsString> {
 }
 
 register_module!(mut cx, {
-    cx.export_function("validateSingleMsgValue", validate_single_message_value)?;
-    cx.export_function("validateMsgValueChain", validate_message_value)?;
-    cx.export_function("validateMsgChain", validate_message_chain)?;
+    cx.export_function("validateFirstMsgValue", validate_single_message_value)?;
+    cx.export_function("validateMsgValue", validate_message_value)?;
     cx.export_function("countArrayItems", array_item_count)?;
     cx.export_function("validateMsgArray", validate_message_array)?;
     Ok(())
