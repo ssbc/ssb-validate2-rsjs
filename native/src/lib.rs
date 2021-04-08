45983f15fab3af91052b9f2256a1507963e74898
mycognosist <gnomad@cryptolab.net>
2021-04-08 10:34:53 +0000

    Remove unnecessary functions

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -6,52 +6,6 @@ use arrayvec::ArrayVec;
 use neon::prelude::*;
 use ssb_validate;
 
-// Validate a single message value (must be first message of feed).
-// Expects an argument in the form of a JSON object.
-fn validate_single_message_value(mut cx: FunctionContext) -> JsResult<JsString> {
-    let arg1 = cx.argument::<JsValue>(0)?;
-
-    if arg1.is_a::<JsUndefined>() || arg1.is_a::<JsNull>() {
-        panic!("undefined argument(s)")
-    };
-
-    let msg1 = utils::json_stringify(&mut cx, ArrayVec::from([arg1]))?
-        .value()
-        .into_bytes();
-
-    match ssb_validate::validate_message_value_hash_chain::<_, &[u8]>(msg1, None) {
-        Ok(_) => Ok(cx.string("validated")),
-        Err(e) => panic!("{}", e),
-    }
-}
-
-// Validate a pair of message values.
-// Expects two arguments (msg1, msg2) in the form of JSON objects.
-fn validate_message_value(mut cx: FunctionContext) -> JsResult<JsString> {
-    let arg1 = cx.argument::<JsValue>(0)?;
-    let arg2 = cx.argument::<JsValue>(1)?;
-
-    // TODO: improve type checking and error-handling
-    if arg1.is_a::<JsUndefined>() || arg1.is_a::<JsNull>() || arg2.is_a::<JsUndefined>() {
-        panic!("undefined argument(s)")
-    };
-
-    // previous message
-    let msg1 = utils::json_stringify(&mut cx, ArrayVec::from([arg1]))?
-        .value()
-        .into_bytes();
-
-    // current message
-    let msg2 = utils::json_stringify(&mut cx, ArrayVec::from([arg2]))?
-        .value()
-        .into_bytes();
-
-    match ssb_validate::validate_message_value_hash_chain(msg2, Some(msg1)) {
-        Ok(_) => Ok(cx.string("validated")),
-        Err(e) => panic!("{}", e),
-    }
-}
-
 // Validate array of messages.
 fn validate_message_array(mut cx: FunctionContext) -> JsResult<JsString> {
     let js_msgs: Handle<JsArray> = cx.argument(0)?;
@@ -73,8 +27,6 @@ fn validate_message_array(mut cx: FunctionContext) -> JsResult<JsString> {
 }
 
 register_module!(mut cx, {
-    cx.export_function("validateFirstMsgValue", validate_single_message_value)?;
-    cx.export_function("validateMsgValue", validate_message_value)?;
     cx.export_function("validateMsgArray", validate_message_array)?;
     Ok(())
 });
