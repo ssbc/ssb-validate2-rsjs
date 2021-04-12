7fd8fa1f2d5f7e6616e6fb77b34de9d07d9000c8
mycognosist <gnomad@cryptolab.net>
2021-04-12 10:03:53 +0000

    Remove deps and profiling functions

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -5,83 +5,9 @@ mod utils;
 use arrayvec::ArrayVec;
 use neon::prelude::*;
 
-// Implement multiple function variants to test overhead of neon.
-fn do_nothing(mut cx: FunctionContext) -> JsResult<JsString> {
-    Ok(cx.string("validated"))
-}
-
-fn get_arg(mut cx: FunctionContext) -> JsResult<JsString> {
-    let _js_msgs: Handle<JsArray> = cx.argument(0)?;
-    Ok(cx.string("validated"))
-}
-
-fn arg_to_vec(mut cx: FunctionContext) -> JsResult<JsString> {
-    let js_msgs: Handle<JsArray> = cx.argument(0)?;
-    let _msg_array: Vec<Handle<JsValue>> = js_msgs.to_vec(&mut cx)?;
-    Ok(cx.string("validated"))
-}
-
-fn vec_to_bytes(mut cx: FunctionContext) -> JsResult<JsString> {
-    let js_msgs: Handle<JsArray> = cx.argument(0)?;
-    let msg_array: Vec<Handle<JsValue>> = js_msgs.to_vec(&mut cx)?;
-
-    let mut msgs = Vec::new();
-
-    for msg in msg_array {
-        let msg_bytes = utils::json_stringify(&mut cx, ArrayVec::from([msg]))?
-            .value()
-            .into_bytes();
-        msgs.push(msg_bytes)
-    }
-
-    Ok(cx.string("validated"))
-}
-
-// Validate array of messages.
-// TODO: change output to JsResult<JsBoolean> and `throw` on error
-fn validate_message_array(mut cx: FunctionContext) -> JsResult<JsString> {
-    let js_msgs: Handle<JsArray> = cx.argument(0)?;
-    let msg_array: Vec<Handle<JsValue>> = js_msgs.to_vec(&mut cx)?;
-
-    let mut msgs = Vec::new();
-
-    for msg in msg_array {
-        let msg_bytes = utils::json_stringify(&mut cx, ArrayVec::from([msg]))?
-            .value()
-            .into_bytes();
-        msgs.push(msg_bytes)
-    }
-
-    match ssb_validate::par_validate_message_hash_chain_of_feed::<_, &[u8]>(&msgs, None) {
-        Ok(_) => Ok(cx.string("validated")),
-        Err(e) => panic!("{}", e),
-    }
-}
-
-// Verify signatures and validate an array of messages.
-// TODO: change output to JsResult<JsBoolean> and `throw` on error
-fn verify_message_array(mut cx: FunctionContext) -> JsResult<JsString> {
-    let js_msgs: Handle<JsArray> = cx.argument(0)?;
-    let msg_array: Vec<Handle<JsValue>> = js_msgs.to_vec(&mut cx)?;
-
-    let mut msgs = Vec::new();
-
-    for msg in msg_array {
-        let msg_bytes = utils::json_stringify(&mut cx, ArrayVec::from([msg]))?
-            .value()
-            .into_bytes();
-        msgs.push(msg_bytes)
-    }
-
-    match ssb_verify_signatures::par_verify_messages(&msgs, None) {
-        Ok(_) => Ok(cx.string("verified")),
-        Err(e) => panic!("{}", e),
-    }
-}
-
 // Verify signatures for an array of messages.
 // TODO: change output to JsResult<JsBoolean> and `throw` on error
-fn verify_validate_message_array(mut cx: FunctionContext) -> JsResult<JsString> {
+fn verify_validate_message_array(mut cx: FunctionContext) -> JsResult<JsBoolean> {
     let js_msgs: Handle<JsArray> = cx.argument(0)?;
     let msg_array: Vec<Handle<JsValue>> = js_msgs.to_vec(&mut cx)?;
 
@@ -96,19 +22,12 @@ fn verify_validate_message_array(mut cx: FunctionContext) -> JsResult<JsString> 
 
     ssb_verify_signatures::par_verify_messages(&msgs, None).unwrap();
 
-    match ssb_validate::par_validate_message_hash_chain_of_feed::<_, &[u8]>(&msgs, None) {
-        Ok(_) => Ok(cx.string("validated")),
-        Err(e) => panic!("{}", e),
-    }
+    ssb_validate::par_validate_message_hash_chain_of_feed::<_, &[u8]>(&msgs, None).unwrap();
+
+    Ok(cx.boolean(true))
 }
 
 register_module!(mut cx, {
-    cx.export_function("doNothing", do_nothing)?;
-    cx.export_function("getArg", get_arg)?;
-    cx.export_function("argVec", arg_to_vec)?;
-    cx.export_function("vecBytes", vec_to_bytes)?;
-    cx.export_function("validateMsgArray", validate_message_array)?;
-    cx.export_function("verifyMsgArray", verify_message_array)?;
-    cx.export_function("verifyValidateMsgArray", verify_validate_message_array)?;
+    cx.export_function("validateBatch", verify_validate_message_array)?;
     Ok(())
 });
