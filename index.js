ea2bc275a1b3df21a80ab930df664fad7d63fa49
mycognosist <gnomad@cryptolab.net>
2021-04-19 15:58:58 +0200

    Add function wrappers for JSON and previous handling

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-module.exports = require('./dist/index.node');
+const v = require("./dist/index.node");
+
+const verifySignatures = (msgs) => {
+  const jsonMsgs = msgs.map((msg) => {
+    return JSON.stringify(msg, null, 2);
+  });
+  return v.verifySignatures(jsonMsgs);
+};
+
+const validateBatch = (msgs, previous) => {
+  const jsonMsgs = msgs.map((msg) => {
+    return JSON.stringify(msg, null, 2);
+  });
+  if (previous !== undefined) {
+    const jsonPrevious = JSON.stringify(previous, null, 2);
+    return v.validateBatch(jsonMsgs, jsonPrevious);
+  }
+  return v.validateBatch(jsonMsgs);
+};
+
+const validateOOOBatch = (msgs, previous) => {
+  const jsonMsgs = msgs.map((msg) => {
+    return JSON.stringify(msg, null, 2);
+  });
+  if (previous !== undefined) {
+    const jsonPrevious = JSON.stringify(previous, null, 2);
+    return v.validateOOOBatch(jsonMsgs, jsonPrevious);
+  } else {
+    return v.validateOOOBatch(jsonMsgs);
+  }
+};
+
+module.exports.verifySignatures = verifySignatures;
+module.exports.validateBatch = validateBatch;
+module.exports.validateOOOBatch = validateOOOBatch;
