b667ad88e497efb09ccc636411f7af67797b5ef1
Andre Staltz <andre@staltz.com>
2021-06-02 15:40:54 +0300

    remove error capturing from install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,3 @@
-set -eEu -o pipefail
-shopt -s extdebug
-IFS=$'\n\t'
-trap 'onFailure $?' ERR
-
-function onFailure() {
-  echo "Unhandled script error $1 at ${BASH_SOURCE[0]}:${BASH_LINENO[0]}" >&2
-  exit 1
-}
-
 if [ "$npm_config_platform" == "android" ] || [ "$npm_config_platform" == "ios" ]; then
   cd native
   mkdir -p dist
