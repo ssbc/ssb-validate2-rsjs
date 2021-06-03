db0634f2040b01efffcf5b89d2cf4ae80c203f74
Andre Staltz <andre@staltz.com>
2021-06-03 13:10:13 +0300

    fix install.sh for ios

--- install.sh
+++ install.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-if [ "$npm_config_platform" == "android" ] || [ "$npm_config_platform" == "ios" ]; then
+if [ "$npm_config_platform" == "android" ]; then
   cd native
   mkdir -p dist
   cargo build --release
   cd ..
   cp ./target/$CARGO_BUILD_TARGET/release/libssb_validate2_rsjs.so ./native/dist/index.node
+elif [ "$npm_config_platform" == "ios" ]; then
+  cd native
+  mkdir -p dist
+  cargo build --release
+  cd ..
+  cp ./target/$CARGO_BUILD_TARGET/release/libssb_validate2_rsjs.dylib ./native/dist/index.node
 fi
