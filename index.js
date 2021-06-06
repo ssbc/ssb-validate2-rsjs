1905e27d5efb8df57dbd86226c7f8990f533926d
Andre Staltz <andre@staltz.com>
2021-06-06 12:31:49 +0300

    rename -web -to -wasm in index.js

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 if (typeof localStorage === "undefined" || localStorage === null)
   module.exports = require("ssb-validate2-rsjs-node");
-else module.exports = require("ssb-validate2-rsjs-web");
+else module.exports = require("ssb-validate2-rsjs-wasm");
