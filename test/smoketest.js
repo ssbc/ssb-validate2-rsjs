37cadd1e945239ace4298706f04a1c85128b5a85
Andre Staltz <andre@staltz.com>
2021-06-07 12:56:32 +0300

    fix smoketest

--- test/smoketest.js
+++ test/smoketest.js
@@ -4,7 +4,7 @@ const validate = require("../");
 test("validateSingle is available in node.js", (t) => {
   const out = validate.validateSingle({foo: 32})
   t.ok(out)
-  t.equals(out, 1285)
+  t.match(out, /found invalid message/)
   t.end()
 });
 
